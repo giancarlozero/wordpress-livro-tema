36efbb6167299ea9dd78d0f037453b6a74ad339c
Giancarlo Silva <giancarlozero@gmail.com>
2016-11-28 20:10:08 -0300

    Branch para o tutorial do livro criada.

--- sidebar.php
+++
@@ -1,8 +0,0 @@
-<aside>
-    <div class="lateral small-12 medium-4 large-4 column"> <!-- Barra lateral -->
-        <div class="widget row">
-            <?php dynamic_sidebar('widgets-barra-lateral'); ?>
-        </div>
-    </div>
-</aside>
-<?php get_footer(); ?>
